baa39524d861adf7b45d3c47ee0a3f09b9095a91
jebibot <83044352+jebibot@users.noreply.github.com>
2024-02-05 02:23:50 +0900

    feat: css

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import fs from "fs";
 import got from "got";
 import beautify from "js-beautify";
+import * as prettier from "prettier";
 
 async function getScript(url, name) {
   const page = await got(url);
@@ -12,8 +13,19 @@ async function getScript(url, name) {
   return scriptSrc[2];
 }
 
+async function getCSS(url, name) {
+  const page = await got(url);
+  const cssSrc = page.body.match(/href="([^"]+\/main\.([0-9a-f]+)\.css)"/);
+  console.log(name, cssSrc[2]);
+  const mainCss = await got(cssSrc[1]);
+  const beautified = await prettier.format(mainCss.body, { parser: "css" });
+  await fs.promises.writeFile(name, beautified);
+  return cssSrc[2];
+}
+
 const scripts = await Promise.all([
   getScript("https://chzzk.naver.com/", "chzzk.js"),
   getScript("https://studio.chzzk.naver.com/", "studio.js"),
+  getCSS("https://chzzk.naver.com/", "chzzk.css"),
 ]);
 await fs.promises.writeFile("message.txt", scripts.join(" "));
